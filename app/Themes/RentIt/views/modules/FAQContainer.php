99d2ded0b39d4c794ff6574244d28217a95447a3
Heeto Bha <99505542+Heetobha@users.noreply.github.com>
2024-01-21 16:15:06 +0500

    add form in the faq page

--- app/Themes/RentIt/views/modules/FAQContainer.php
+++ app/Themes/RentIt/views/modules/FAQContainer.php
@@ -1,3 +1,12 @@
+<style>
+    .page-section.contact {
+        background-color: white !important;   
+    }
+    .page-section{
+        padding-bottom: 40px !important;
+    }
+</style>
+
 <section id="<?php echo $id ?? 0 ?>" class="page-section with-sidebar sub-page pb-module-section">
     <div class="container">
         <div class="row col-md-8">
@@ -20,3 +29,54 @@
         </div>
     </div>
 </section>
+<section id="contact_us_faq__0" class="pb-module-section ui-sortable-handle" >
+    <div class="edit container" >
+
+        <div class="edit row" >
+            <div class="edit col-md-6" >
+                <!-- Contact form -->
+                <form name="contact-form" method="post" action="#" class="edit contact-form alt" id="contact-form" >
+
+                    <div class="edit row" >
+                        <div class="edit col-md-6" >
+
+                            <div class="edit outer required" >
+                                <div class="edit form-group af-inner has-icon" >
+                                    <label class="edit sr-only" for="name" >Name</label>
+                                    <input type="text" name="name" id="name" placeholder="Name" value="" size="30" data-toggle="tooltip" title="" class="edit form-control placeholder" data-original-title="Name is required" >
+                                    <span class="edit form-control-icon" ><i class="edit fa fa-user" ></i></span>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="edit col-md-6" >
+
+                            <div class="edit outer required" >
+                                <div class="edit form-group af-inner has-icon" >
+                                    <label class="edit sr-only" for="email" >Email</label>
+                                    <input type="text" name="email" id="email" placeholder="Email" value="" size="30" data-toggle="tooltip" title="" class="edit form-control placeholder" data-original-title="Email is required" >
+                                    <span class="edit form-control-icon" ><i class="edit fa fa-envelope" ></i></span>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="edit form-group af-inner has-icon" >
+                        <label class="edit sr-only" for="input-message" > Please Give us breif about the cooperation </label>
+                        <textarea name="message" id="input-message" placeholder=" Please Give us breif about the cooperation" rows="4" cols="50" data-toggle="tooltip" title="" class="edit form-control placeholder" data-original-title="Message is required" ></textarea>
+                        <span class="edit form-control-icon" ><i class="edit fa fa-bars" ></i></span>
+                    </div>
+
+                    <div class="edit outer required" >
+                        <div class="edit form-group af-inner" >
+                            <input type="submit" name="submit" class="edit form-button form-button-submit btn btn-block btn-theme" id="submit_btn" value="Submit" >
+                        </div>
+                    </div>
+
+                </form>
+                <!-- /Contact form -->
+
+        <!-- /Get in touch -->
+
+    </div></section>
